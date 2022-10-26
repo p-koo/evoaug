b83b888cd443ee1c0649f10284bb9390a02bdfac
koo-lab <koolaboratory.gmail.com>
2022-10-26 09:53:07 -0400

    updates

--- evoaug/utils.py
+++ evoaug/utils.py
@@ -7,65 +7,13 @@ from sklearn.metrics import roc_auc_score, average_precision_score, mean_squared
 from scipy import stats
 
 
+#------------------------------------------------------------------------
+# useful functions
+#------------------------------------------------------------------------
 
 
-class H5DataModule(pl.LightningDataModule):
-    def __init__(self, data_path, batch_size=128, stage=None, lower_case=False, transpose=False):
-        super().__init__()
-        self.data_path = data_path
-        self.batch_size = batch_size
-        self.x = 'X'
-        self.y = 'Y'
-        if lower_case:
-            self.x = 'x'
-            self.y = 'y'
-        self.transpose = transpose
-        self.setup(stage)
-
-    def setup(self, stage=None):
-        # Assign train and val split(s) for use in DataLoaders
-        if stage == "fit" or stage is None:
-            with h5py.File(self.data_path, 'r') as dataset:
-                x_train = np.array(dataset[self.x+"_train"]).astype(np.float32)
-                x_valid = np.array(dataset[self.x+"_valid"]).astype(np.float32)
-                if self.transpose:
-                    x_train = np.transpose(x_train, (0,2,1))
-                    x_valid = np.transpose(x_valid, (0,2,1))
-
-                self.x_train = torch.from_numpy(x_train)
-                self.y_train = torch.from_numpy(np.array(dataset[self.y+"_train"]).astype(np.float32))
-                self.x_valid = torch.from_numpy(x_valid)
-                self.y_valid = torch.from_numpy(np.array(dataset[self.y+"_valid"]).astype(np.float32))
-            _, self.A, self.L = self.x_train.shape # N = number of seqs, A = alphabet size (number of nucl.), L = length of seqs
-            self.num_classes = self.y_train.shape[1]
-            
-        # Assign test split(s) for use in DataLoaders
-        if stage == "test" or stage is None:
-            with h5py.File(self.data_path, "r") as dataset:
-                x_test = np.array(dataset[self.x+"_test"]).astype(np.float32)
-                if self.transpose:
-                    x_test = np.transpose(x_test, (0,2,1))
-                self.x_test = torch.from_numpy(x_test)
-                self.y_test = torch.from_numpy(np.array(dataset[self.y+"_test"]).astype(np.float32))
-            _, self.A, self.L = self.x_train.shape
-            self.num_classes = self.y_train.shape[1]
-            
-    def train_dataloader(self):
-        train_dataset = TensorDataset(self.x_train, self.y_train) # tensors are index-matched
-        return DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True) # sets of (x, x', y) will be shuffled
-    
-    def val_dataloader(self):
-        valid_dataset = TensorDataset(self.x_valid, self.y_valid) 
-        return DataLoader(valid_dataset, batch_size=self.batch_size, shuffle=False)
-    
-    def test_dataloader(self):
-        test_dataset = TensorDataset(self.x_test, self.y_test)
-        return DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False) 
-
-
-
-def get_predictions(model, x, batch_size=100):
-    trainer = pl.Trainer(gpus=1)
+def get_predictions(model, x, batch_size=100, gpus=1):
+    trainer = pl.Trainer(gpus=gpus, logger=None)
     dataloader = torch.utils.data.DataLoader(x, batch_size=batch_size, shuffle=False) 
     pred = trainer.predict(model, dataloaders=dataloader)
     return np.concatenate(pred)
@@ -142,6 +90,67 @@ def configure_optimizer(model, lr=0.001, weight_decay=1e-6, decay_factor=0.1, pa
         },
     }
 
+
+
+#------------------------------------------------------------------------
+# useful functions
+#------------------------------------------------------------------------
+
+
+
+
+class H5DataModule(pl.LightningDataModule):
+    def __init__(self, data_path, batch_size=128, stage=None, lower_case=False, transpose=False):
+        super().__init__()
+        self.data_path = data_path
+        self.batch_size = batch_size
+        self.x = 'X'
+        self.y = 'Y'
+        if lower_case:
+            self.x = 'x'
+            self.y = 'y'
+        self.transpose = transpose
+        self.setup(stage)
+
+    def setup(self, stage=None):
+        # Assign train and val split(s) for use in DataLoaders
+        if stage == "fit" or stage is None:
+            with h5py.File(self.data_path, 'r') as dataset:
+                x_train = np.array(dataset[self.x+"_train"]).astype(np.float32)
+                x_valid = np.array(dataset[self.x+"_valid"]).astype(np.float32)
+                if self.transpose:
+                    x_train = np.transpose(x_train, (0,2,1))
+                    x_valid = np.transpose(x_valid, (0,2,1))
+
+                self.x_train = torch.from_numpy(x_train)
+                self.y_train = torch.from_numpy(np.array(dataset[self.y+"_train"]).astype(np.float32))
+                self.x_valid = torch.from_numpy(x_valid)
+                self.y_valid = torch.from_numpy(np.array(dataset[self.y+"_valid"]).astype(np.float32))
+            _, self.A, self.L = self.x_train.shape # N = number of seqs, A = alphabet size (number of nucl.), L = length of seqs
+            self.num_classes = self.y_train.shape[1]
+            
+        # Assign test split(s) for use in DataLoaders
+        if stage == "test" or stage is None:
+            with h5py.File(self.data_path, "r") as dataset:
+                x_test = np.array(dataset[self.x+"_test"]).astype(np.float32)
+                if self.transpose:
+                    x_test = np.transpose(x_test, (0,2,1))
+                self.x_test = torch.from_numpy(x_test)
+                self.y_test = torch.from_numpy(np.array(dataset[self.y+"_test"]).astype(np.float32))
+            _, self.A, self.L = self.x_train.shape
+            self.num_classes = self.y_train.shape[1]
+            
+    def train_dataloader(self):
+        train_dataset = TensorDataset(self.x_train, self.y_train) # tensors are index-matched
+        return DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True) # sets of (x, x', y) will be shuffled
+    
+    def val_dataloader(self):
+        valid_dataset = TensorDataset(self.x_valid, self.y_valid) 
+        return DataLoader(valid_dataset, batch_size=self.batch_size, shuffle=False)
+    
+    def test_dataloader(self):
+        test_dataset = TensorDataset(self.x_test, self.y_test)
+        return DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False) 
 
 
 
