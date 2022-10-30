b6d78d2edba6fc02baded422659f5ff886f98529
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-10-30 19:43:53 -0400

    add a top-level docstring for evoaug

---
+++ evoaug/__init__.py
@@ -0,0 +1,5 @@
+"""
+EvoAug is a PyTorch package to pretrain sequence-based deep learning models for
+regulatory genomics data with evolution-inspired data augmentations followed by a
+finetuning on the original, unperturbed sequence data.
+"""
