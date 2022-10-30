e0a9dea2507929a8a9b200d4a4cde89ae0ad1a35
koo-lab <koolaboratory.gmail.com>
2022-10-30 14:59:22 -0400

    updates

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="evoaug",
     version="0.1.1",
     packages=find_packages(),
+    description = "A Python package that trains models with evolution-inspired data augmentations. ",
     python_requires=">=3.6",
     install_requires=[
         'pytorch', 
