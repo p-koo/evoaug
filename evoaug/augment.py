ed34e3516e527c1608bc65ffcfb6f80da33a67fb
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-10-30 16:20:40 -0400

    format code block in docstring + rm whitespace

--- evoaug/augment.py
+++ evoaug/augment.py
@@ -1,9 +1,10 @@
-import torch
 """
-Library of data augmentations for genomic sequence data. 
+Library of data augmentations for genomic sequence data.
 
 To contribute a custom augmentation, use the following syntax:
-```
+
+.. code-block:: python
+
     class CustomAugmentation(AugmentBase):
         def __init__(self, param1, param2, ..., paramN):
             self.param1 = param1
@@ -16,12 +17,14 @@ To contribute a custom augmentation, use the following syntax:
             # perform augmentation
 
             return x_aug
-```
 
 """
 
+import torch
+
+
 class AugmentBase():
-    """ 
+    """
     Base clas for EvoAug augmentations for genomic sequences.
     """
     def __call__(self, x):
@@ -29,7 +32,7 @@ class AugmentBase():
 
 
 class RandomDeletion(AugmentBase):
-    """Randomly deletes a contiguous stretch of nucleotides from sequences in a training 
+    """Randomly deletes a contiguous stretch of nucleotides from sequences in a training
     batch according to a random number between a user-defined delete_min and delete_max.
     A different deletion is applied to each sequence.
 
@@ -53,29 +56,29 @@ class RandomDeletion(AugmentBase):
         :return: sequences with randomly deleted segments (padded to correct shape with random DNA)
 
         """
-        N, A, L = x.shape   
+        N, A, L = x.shape
 
         # sample random DNA
         a = torch.eye(A)
         p = torch.tensor([1/A for _ in range(A)])
         padding = torch.stack([a[p.multinomial(self.delete_max, replacement=True)].transpose(0,1) for _ in range(N)]).to(x.device)
-        
+
         # sample deletion length for each sequence
         delete_lens = torch.randint(self.delete_min, self.delete_max + 1, (N,))
 
         # sample locations to delete for each sequence
         delete_inds = torch.randint(L - self.delete_max + 1, (N,)) # deletion must be in boundaries of seq.
-        
+
         # loop over each sequence
         x_aug = []
         for seq, pad, delete_len, delete_ind in zip(x, padding, delete_lens, delete_inds):
 
             # get index of half delete_len (to pad random DNA at beginning of sequence)
             pad_begin_index = torch.div(delete_len, 2, rounding_mode='floor').item()
-            
+
             # index for other half (to pad random DNA at end of sequence)
             pad_end_index = delete_len - pad_begin_index
-            
+
             # removes deletion and pads beginning and end of sequence with random DNA to ensure same length
             x_aug.append( torch.cat([pad[:,:pad_begin_index],                # random dna padding
                                      seq[:,:delete_ind],                     # sequence up to deletion start index
@@ -89,7 +92,7 @@ class RandomDeletion(AugmentBase):
 
 
 class RandomInsertion(AugmentBase):
-    """Randomly inserts a contiguous stretch of nucleotides from sequences in a training 
+    """Randomly inserts a contiguous stretch of nucleotides from sequences in a training
     batch according to a random number between a user-defined insert_min and insert_max.
     A different insertions is applied to each sequence. Each sequence is padded with random
     DNA to ensure same shapes.
@@ -136,7 +139,7 @@ class RandomInsertion(AugmentBase):
 
             # index for other half (to pad random DNA at end of sequence)
             insert_end_len = self.insert_max - insert_len - insert_beginning_len
-    
+
             # removes deletion and pads beginning and end of sequence with random DNA to ensure same length
             x_aug.append( torch.cat([insertion[:,:insert_beginning_len],                                # random dna padding
                                      seq[:,:insert_ind],                                                # sequence up to insertion start index
@@ -150,9 +153,9 @@ class RandomInsertion(AugmentBase):
 
 
 class RandomTranslocation(AugmentBase):
-    """Randomly cuts sequence in two pieces and shifts the order for each in a training 
-    batch. This is implemented with a roll transformation with a user-defined shift_min 
-    and shift_max. A different roll (positive or negative) is applied to each sequence. 
+    """Randomly cuts sequence in two pieces and shifts the order for each in a training
+    batch. This is implemented with a roll transformation with a user-defined shift_min
+    and shift_max. A different roll (positive or negative) is applied to each sequence.
     Each sequence is padded with random DNA to ensure same shapes.
 
     :param shift_min: Minimum size for random shift, defaults to 0
@@ -194,9 +197,9 @@ class RandomTranslocation(AugmentBase):
 
 
 class RandomInversion(AugmentBase):
-    """Randomly inverts a contiguous stretch of nucleotides from sequences in a training 
-    batch according to a user-defined invert_min and invert_max. A different insertions 
-    is applied to each sequence. Each sequence is padded with random DNA to ensure same 
+    """Randomly inverts a contiguous stretch of nucleotides from sequences in a training
+    batch according to a user-defined invert_min and invert_max. A different insertions
+    is applied to each sequence. Each sequence is padded with random DNA to ensure same
     shapes.
 
     :param invert_min: Minimum size for random insertion, defaults to 0
@@ -232,15 +235,15 @@ class RandomInversion(AugmentBase):
         for seq, inversion_len, inversion_ind in zip(x, inversion_lens, inversion_inds):
             x_aug.append( torch.cat([seq[:,:inversion_ind],    # sequence up to inversion start index
                                      torch.flip(seq[:,inversion_ind:inversion_ind+inversion_len], dims=[0,1]), # reverse-complement transformation
-                                     seq[:,inversion_ind+inversion_len:]], # sequence after inversion 
+                                     seq[:,inversion_ind+inversion_len:]], # sequence after inversion
                                     -1)) # concatenation axis
         return torch.stack(x_aug)
 
-        
+
 
 class RandomMutation(AugmentBase):
     """Randomly mutates sequences in a training batch according to a user-defined mutate_frac.
-    A different set of mutations is applied to each sequence. 
+    A different set of mutations is applied to each sequence.
 
     :param mutate_frac: probability of mutation for each nucleotide, defaults to 0.1
     :type float
@@ -261,17 +264,17 @@ class RandomMutation(AugmentBase):
         """
         N, A, L = x.shape
 
-        # determine the number of mutations per sequence 
+        # determine the number of mutations per sequence
         num_mutations = round(self.mutate_frac / 0.75 * L) # num. mutations per sequence (accounting for silent mutations)
 
-        # randomly determine the indices to apply mutations 
+        # randomly determine the indices to apply mutations
         mutation_inds = torch.argsort(torch.rand(N,L))[:, :num_mutations] # see <https://discuss.pytorch.org/t/torch-equivalent-of-numpy-random-choice/16146>0
 
         # create random DNA (to serve as random mutations)
         a = torch.eye(A)
         p = torch.tensor([1/A for _ in range(A)])
         mutations = torch.stack([a[p.multinomial(num_mutations, replacement=True)].transpose(0,1) for _ in range(N)]).to(x.device)
-        
+
         # make a copy of the batch of sequences
         x_aug = torch.clone(x)
 
@@ -283,9 +286,9 @@ class RandomMutation(AugmentBase):
 
 
 class RandomRC(AugmentBase):
-    """Randomly applies a reverse-complement transformation to each sequence in a training 
+    """Randomly applies a reverse-complement transformation to each sequence in a training
     batch according to a user-defined probability, rc_prob. This is applied to each sequence
-    independently. 
+    independently.
 
     :param rc_prob: probility to apply a reverse-complement transformation, defaults to 0.5
     :type float
@@ -306,20 +309,20 @@ class RandomRC(AugmentBase):
         """
 
         # make a copy of the sequence
-        x_aug = torch.clone(x)  
+        x_aug = torch.clone(x)
 
         # randomly select sequences to apply rc transformation
         ind_rc = torch.rand(x_aug.shape[0]) < self.rc_prob
 
         # apply reverse-complement transformation
-        x_aug[ind_rc] = torch.flip(x_aug[ind_rc], dims=[1,2])   
+        x_aug[ind_rc] = torch.flip(x_aug[ind_rc], dims=[1,2])
         return x_aug
 
 
 
 class RandomNoise(AugmentBase):
-    """Randomly add Gaussian noise to a batch of sequences with according to a user-defined 
-    noise_mean and noise_std. A different set of noise is applied to each sequence. 
+    """Randomly add Gaussian noise to a batch of sequences with according to a user-defined
+    noise_mean and noise_std. A different set of noise is applied to each sequence.
 
     :param noise_mean: Bias of the noise -- mean of Gaussian, defaults to 0.0
     :type float
@@ -342,15 +345,3 @@ class RandomNoise(AugmentBase):
 
         """
         return x + torch.normal(self.noise_mean, self.noise_std, x.shape).to(x.device)
-
-
-
-
-
-
-
-
-
-
-
-
